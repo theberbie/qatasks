d4ce2fc6b9d113d7ba57ac5360095b7cf5ca5f1e
vagrant <vagrant@precise32.(none)>
2016-05-29 06:05:13 +0000

    Added security reqs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  validates_format_of :email, with: /\@kabaminc\.com/, message: 'You should have an email from Kabam'
   has_many :tasks         
+
+  validate :password_complexity
+
+   def password_complexity
+    if password.present? and not password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d). /)
+      errors.add :password, " must include at least one lowercase letter, one uppercase letter, and one digit"
+    end
+  end
+
 end
