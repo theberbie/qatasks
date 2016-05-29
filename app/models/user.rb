1d7837721179731e7d275c841c7c0d31781d483a
vagrant <vagrant@precise32.(none)>
2016-05-29 06:49:41 +0000

    Added placeholders

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,6 @@ class User < ActiveRecord::Base
   validates_format_of :email, with: /\@kabaminc\.com/, message: 'You should have an email from Kabam'
   has_many :tasks         
 
-  validate :password_complexity
-
-   def password_complexity
-    if password.present? and not password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d). /)
-      errors.add :password, " must include at least one lowercase letter, one uppercase letter, and one digit"
-    end
-  end
+  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Qatasks::Application.routes.draw do
   devise_for :users
   root 'tasks#index'
   resources :tasks
+  resources :faqs
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
