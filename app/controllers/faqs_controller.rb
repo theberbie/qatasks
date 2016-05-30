4d27e98a0562d48d87c6cb8807ec60917f594335
vagrant <vagrant@precise32.(none)>
2016-05-30 20:56:05 +0000

    Edited fonts

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -15,6 +15,37 @@ redirect_to faqs_path
 
 end
 
+def edit 
+  @faq = Faq.find(params[:id])
+
+end
+
+def show
+  @faq = Faq.find(params[:id])
+end
+
+def update
+  @faq = Faq.find(params[:id])
+  if @faq.user != current_user
+    return render text: 'Not Allowed', status: :forbidden
+  end
+  @faq.update_attributes(faq_params)
+  if @faq.valid?
+  redirect_to faqs_path
+else
+  render :edit, status: :unprocessable_entity
+end
+end
+
+def destroy
+  @faq = Faq.find(params[:id])
+  if @faq.user !=current_user
+    return render text: 'Not Allowed', status: :forbidden
+  end
+
+  @faq.destroy
+  redirect_to faqs_path
+end
 
 private
 
